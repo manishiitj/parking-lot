dd16ffdef4e101f3e3c7894d593246d0eafaf88f
Manish <manish.kumawat@olacabs.com>
2017-07-16 18:20:55 +0530

    adding utility methods

---
+++ src/main/java/parking/util/Contant.java
@@ -0,0 +1,7 @@
+package parking.contant;
+
+/**
+ * Created by Manish on 7/16/17.
+ */
+public class Contant {
+}
---
+++ src/main/java/parking/util/UtitlityHelper.java
@@ -0,0 +1,7 @@
+package parking.util;
+
+/**
+ * Created by Manish on 7/16/17.
+ */
+public class UtitlityHelper {
+}
